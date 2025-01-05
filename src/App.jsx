cc7fac624125931c26cb8c75a504e5bf84297345
Alex Bradau <alexbradau@gmail.com>
2025-01-05 16:39:56 -0500

    Created reusable button

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,17 @@
-import { useState } from "react";
+import React from 'react';
+import ButtonGradient from "./assets/svg/ButtonGradient";
+import Button from './components/Button';
 
-function App() {
-  const [count, setCount] = useState(0);
-
-  return <h1 className="text-3xl font-bold underline">Hello world!</h1>;
-}
+const App =() => {
+  return(
+    <>
+      <h1 className="text-3xl font-bold underline">Hello world!</h1>
+      <div className="pt-[4.75rem] lg:pt-[5.25rem] overflow-hidden">
+        <Button className="mt-10" href="#login">Click me!</Button>
+      </div>
+      <ButtonGradient />
+    </>
+  );
+};
 
 export default App;
