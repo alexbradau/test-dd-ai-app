263f9fe42fa6969355651d060cd74b62e96ba11e
Alex Bradau <alexbradau@gmail.com>
2025-01-18 22:52:23 -0500

    Wrote benefits section

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 import ButtonGradient from "./assets/svg/ButtonGradient";
 import Header from './components/Header';
 import Landing from './components/Landing'; 
+import Benefits from './components/Benefits';
 
 const App =() => {
   return(
@@ -9,6 +10,7 @@ const App =() => {
       <div className="pt-[4.75rem] lg:pt-[5.25rem] overflow-hidden">
         <Header/>
         <Landing/>
+        <Benefits/>
       </div>
       <ButtonGradient />
     </>
