2bddd27e9d50d4ad731322992409e91c77cf92d3
Alex Bradau <alexbradau@gmail.com>
2025-02-08 14:30:18 -0500

    temporarily removed New Account and Sign In links

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -89,14 +89,15 @@ useEffect(() => {
           </div>
 
         </nav>
-
         <Link to="/demo" className="button hidden mr-8 text-n-1/50 transition-colors hover:text-n-1 lg:block">Demo</Link>
-
+        { false && (
         <Link to="/signup" className="button hidden mr-8 text-n-1/50 transition-colors hover:text-n-1 lg:block">New Account</Link>
+        )}
+        { false && (
         <Button className="hidden lg:flex" href="#login">
           Sign in
         </Button>
-
+        )}
         <Button
           className="ml-auto lg:hidden"
           px="px-3"
